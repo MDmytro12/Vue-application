64f0fbaddfd1333fa682949d97f27e538bb74d64
Dimasta <dimas1969592@gmail.com>
2021-08-07 18:24:36 +0300

    Finish creating a new category!

---
+++ src/store/category.js
@@ -0,0 +1,23 @@
+import firebase from "firebase/app"
+
+export default {
+    actions: {
+        async createCategory({dispatch , commit} , {title , limit} ){
+            try{
+                const uid = await dispatch('getUserId')
+                const category = await firebase.database().ref(`/users/${uid}/categories`).push({
+                    title ,
+                    limit
+                })
+
+                return {
+                    title ,
+                    limit,
+                    id: category.key
+                }
+            }catch (e) {
+                throw e
+            }
+        }
+    }
+}
